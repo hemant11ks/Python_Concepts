3aeec76cec0626ec5cf395fcae607b2558dfc740
Hemant Kumar Singh <ksingh1617@gmail.com>
2025-03-27 23:32:28 +0530

    Add files via upload

--- Python_advance_LnkLearning/Ch_1/assign_start.py
+++ Python_advance_LnkLearning/Ch_1/assign_start.py
@@ -1,2 +1,42 @@
-# String: Is the sequence of unicode characters
-# Bytes: Is the sequence of raw eight bit values
+import pprint
+
+# Regular assignment statement assign a value
+# x = 5
+# print(x)
+
+# Assignment operator is part of an expression
+# It evaluates to the value on the right
+
+# ( x:= 10 )
+# print(x)
+# 10
+
+# Assignment expression is usefull for writting concise code
+# thestr = input("Give me a string: ")
+# while thestr != "exit":
+#     print(thestr)
+#     thestr = input("Give me a string: ")
+
+# Give me a string: hemant
+# hemant
+# Give me a string: exit
+
+# Here in this example we are calling input two times which decreases the performance of the code.
+# while(thestr := input("Give me a string:")) != "exit":
+#     print(thestr)
+
+# The walrus operator can help reduce the number of same function calls.
+
+values = [1,2,3,4,5,6,7,8,9,10]
+# val_data = {
+#     "length": len(values),
+#     "total": sum(values),
+#     "mean": sum(values)/len(values),
+# }
+
+# print(val_data)
+# {'length': 10, 'total': 55, 'mean': 5.5}
+
+# Now lets use walrus operator to do the same thing
+if (n:= len(values))>0:
+    print(f"Mean: {sum(values)/len(values)} | Length: {len(values)} | Total: {sum(values)}")
--- Python_advance_LnkLearning/Ch_1/string_start.py
+++ Python_advance_LnkLearning/Ch_1/string_start.py
@@ -1,2 +1,26 @@
 # String: Is the sequence of unicode characters
 # Bytes: Is the sequence of raw eight bit values
+
+b = bytes([0x41, 0x42, 0x43, 0x44])
+print(b)
+
+s = "abcd"
+print(s)
+
+# It will not work as we need to first decode te byte
+# print(b+s)
+
+s2 = b.decode("utf-8")
+print(s+s2)
+# abcdABCD
+
+
+# Now lets learn how to encode a string into bytes
+s3 = "This is a test"
+b2 = s3.encode("utf-8")
+print(b2)
+# b'This is a test'
+
+b3 = s.encode("utf-32")
+print(b3)
+# b'\xff\xfe\x00\x00a\x00\x00\x00b\x00\x00\x00c\x00\x00\x00d\x00\x00\x00'
